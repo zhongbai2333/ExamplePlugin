2ff17840b6f0a19eeda70daef9ab70ce2b1efffd
zhong_bai <54356787+zhongbai2333@users.noreply.github.com>
2025-01-25 20:23:01 +0800

    Fix: Lost mcdr: PluginServerInterface

--- connect_core/api/mcdr/__init__.py
+++ connect_core/api/mcdr/__init__.py
@@ -1,12 +1,17 @@
 from connect_core.api.interface import PluginControlInterface
+from mcdreforged.api.types import PluginServerInterface
 
-def get_plugin_control_interface(sid: str, enter_point: str) -> PluginControlInterface:
+
+def get_plugin_control_interface(
+    sid: str, enter_point: str, mcdr: PluginServerInterface
+) -> PluginControlInterface:
     """
     获取插件控制接口
 
     Args:
         sid (str): 插件ID
         enter_point (str): 入口点
+        mcdr (PluginServerInterface): MCDR接口
     Returns:
         PluginControlInterface: 插件控制接口
     """
